6aaf47349c52187ae7c661c20fc46588553b5376
uqse17157 <115668377+uqse17157@users.noreply.github.com>
2022-10-26 21:21:24 +0100

    Update group.py

--- group.py
+++ group.py
@@ -1,5 +1,18 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+names = ['Jill', 'Zalika', 'John', 'Nash']
+ages = [26, 28, 27, 34]
+jobs = ['biologist', 'artist', 'writer', 'chef']
+networks = [{'friend': 'Zalika', 'partner': 'John'},{'friend': 'Jill'},{'partner': 'Jill'},{'cousin':'John', 'landlord':'Zalika'}]
 
-my_group =
+my_group = []
+
+def add_people(names, ages, jobs, networks):
+    for i in range(len(names)):
+        my_group.append({ 'name': names[i], 'age': ages[i], 'job': jobs[i], 'network': networks[i]})
+    return my_group
+
+my_group = add.people(names, ages, jobs, networks)
+
+print(my_group)
